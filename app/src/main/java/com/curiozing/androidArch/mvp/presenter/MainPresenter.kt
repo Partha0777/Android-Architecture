514a287c4312523c28c1daab31514911cdb3e9d8
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-15 22:49:29 +0530

    Added simple example for MVP pattern

---
+++ app/src/main/java/com/curiozing/androidArch/mvp/presenter/MainPresenter.kt
@@ -0,0 +1,14 @@
+package com.curiozing.androidArch.mvp.presenter
+
+class MainPresenter(private var mainView: MainView) {
+
+
+    fun apiCall(){
+        mainView.updateUI()
+    }
+
+
+    interface MainView{
+        fun updateUI()
+    }
+}
---
+++ app/src/main/java/com/curiozing/androidArch/mvp/view/MvpMainActivity.kt
@@ -0,0 +1,21 @@
+package com.curiozing.androidArch.mvp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.curiozing.androidArch.R
+import com.curiozing.androidArch.mvp.presenter.MainPresenter
+
+class MvpMainActivity : AppCompatActivity(), MainPresenter.MainView {
+
+
+    lateinit var mainPresenter: MainPresenter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mvp_main)
+        mainPresenter = MainPresenter(this)
+    }
+
+    override fun updateUI() {
+
+    }
+}
