58247ba7ab3ccb593d82cc9d5c81c03d64709d0c
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-16 22:27:38 +0530

    Added viewModel delegate for main viewModel in MVVM pattern,

--- app/src/main/java/com/curiozing/androidArch/mvvm/view/MvvmMainActivity.kt
+++ app/src/main/java/com/curiozing/androidArch/mvvm/view/MvvmMainActivity.kt
@@ -2,17 +2,16 @@ package com.curiozing.androidArch.mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.curiozing.androidArch.R
 import com.curiozing.androidArch.mvvm.viewModel.MainViewModel
 
 class MvvmMainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mvvm_main)
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
     }
 }
