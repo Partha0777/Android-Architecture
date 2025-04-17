b4f0a6acf6848d3bb560250068c9c0b10290fd95
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-17 22:36:31 +0530

    Added viewModel delegate for main viewModel in MVVM pattern,

--- app/src/main/java/com/curiozing/androidArch/mvvm/viewModel/MainViewModel.kt
+++ app/src/main/java/com/curiozing/androidArch/mvvm/viewModel/MainViewModel.kt
@@ -2,9 +2,9 @@ package com.curiozing.androidArch.mvvm.viewModel
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel(){
+class MainViewModel : ViewModel() {
 
-    fun callApi(){
+    fun callApi() {
 
     }
 }
