2b3ae79c7efa72e1d8349d4e173a9dda82e26a22
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-20 22:55:42 +0530

    Added viewModel delegate for main viewModel in MVVM pattern,

---
+++ app/src/main/java/com/curiozing/androidArch/cleanArch/model/CleanArchMainActivity.kt
@@ -0,0 +1,12 @@
+package com.curiozing.androidArch.cleanArch.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.curiozing.androidArch.R
+
+class CleanArchMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clean_arch_main)
+    }
+}
