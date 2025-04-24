46df0da1a5e71331d26df03ac41f4d274c4364e3
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-24 23:15:52 +0530

    Added repo for the image downloader.

--- app/src/main/java/com/curiozing/androidArch/cleanArch/model/model/ImageModel.kt
+++ app/src/main/java/com/curiozing/androidArch/cleanArch/model/model/ImageModel.kt
@@ -1,6 +1,5 @@
 package com.curiozing.androidArch.cleanArch.model.model
 
-import androidx.lifecycle.ViewModel
 import com.curiozing.androidArch.cleanArch.domain.entity.ImageEntity
 
 class ImageModel : ImageEntity() {
