043ac21b187c95af44f1e496906410e237b4b192
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-25 23:28:44 +0530

    Added repo for the image downloader.

--- app/src/main/java/com/curiozing/androidArch/cleanArch/ui/view/CleanArchMainActivity.kt
+++ app/src/main/java/com/curiozing/androidArch/cleanArch/ui/view/CleanArchMainActivity.kt
@@ -2,9 +2,15 @@ package com.curiozing.androidArch.cleanArch.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 import com.curiozing.androidArch.R
+import com.curiozing.androidArch.mvvm.viewModel.MainViewModel
 
 class CleanArchMainActivity : AppCompatActivity() {
+
+    val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clean_arch_main)
