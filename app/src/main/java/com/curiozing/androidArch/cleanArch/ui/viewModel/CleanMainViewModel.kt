2e291a437a6465f3d00a2efb2a72452255875506
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-04-27 22:52:34 +0530

    Added repo for the image downloader.

--- app/src/main/java/com/curiozing/androidArch/cleanArch/ui/viewModel/CleanMainViewModel.kt
+++ app/src/main/java/com/curiozing/androidArch/cleanArch/ui/viewModel/CleanMainViewModel.kt
@@ -1,10 +1,12 @@
 package com.curiozing.androidArch.cleanArch.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.curiozing.androidArch.cleanArch.domain.usecase.ImageUseCase
 
 class CleanMainViewModel : ViewModel() {
+    val imageUseCase = ImageUseCase()
 
     fun callApi(){
-
+        imageUseCase.downloadImage()
     }
 }
