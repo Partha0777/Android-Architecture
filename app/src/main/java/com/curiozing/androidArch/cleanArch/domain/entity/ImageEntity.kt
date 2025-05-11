6307610837d59065da3bba9f02ab6cf83a48730f
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-05-11 17:12:00 +0530

    Added repo for the image downloader.

--- app/src/main/java/com/curiozing/androidArch/cleanArch/domain/entity/ImageEntity.kt
+++ app/src/main/java/com/curiozing/androidArch/cleanArch/domain/entity/ImageEntity.kt
@@ -1,4 +1,12 @@
 package com.curiozing.androidArch.cleanArch.domain.entity
 
-open class ImageEntity {
-}
+data class ImageEntity(
+    val id: String,
+    val url: String,
+    val title: String,
+    val description: String? = null,
+    val width: Int,
+    val height: Int,
+    val createdAt: Long = System.currentTimeMillis(),
+    val date: String
+)
